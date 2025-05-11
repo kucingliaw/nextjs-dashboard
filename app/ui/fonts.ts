3a120b83aee4ce76acc7a3b539e7d154b6718e80
Wawal <wawal.wiguna@gmail.com>
2025-05-11 20:05:58 +0700

    chapter 3

---
+++ app/ui/fonts.ts
@@ -0,0 +1,12 @@
+import { Inter } from "next/font/google";
+import { Lusitana } from "next/font/google";
+
+// Import the Inter font from Google Fonts
+export const inter = Inter({
+  subsets: ["latin"],
+});
+
+export const lusitana = Lusitana({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
